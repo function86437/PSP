0d6bbb15deb15d71c1ad71a1b45ec98300f72dfd
function86437 <function86437@gmail.com>
2015-04-01 04:58:50 +0800

    complete DB connection by PDO

---
+++ PSP/DBConnect.php
@@ -0,0 +1,74 @@
+<?php
+    /**
+    *   Connect to Database by PDO
+    *   @version 0.1
+    **/
+    class DBConnect {
+
+        public $con = null;
+
+        //construct
+        function _construct() {
+            $this->connect();
+        }
+
+        //destruct
+        function _destruct() {
+            $this->close();
+        }
+
+
+        //connect to db
+        private function connect() {
+
+            //import db variables
+            require_once _DIR_ . '/DBConfig.php';
+
+            //connecting to db
+            try{
+                $this->$con = new PDO(DB_SERVER_CONFIG, DB_USER, DB_PASSWORD);
+            } catch (PDOException $e) {
+                printf('DatabaseError: %s', $e->getMessange());
+            }
+
+        }
+
+        /**
+        *   Query
+        *   @param $sql query command
+        *   @param $exepara query data
+        *   @return $stmt
+        **/
+        public function query($sql, $exepara) {
+            //Inorder to avoid sql injection, pdo send twice times for one transaction
+            //First is command
+            $stmt = $this->$con->prepare($sql);
+
+            //Second is data
+            $stmt->execute($exepara);
+
+            return $stmt;
+        }
+
+
+        /**
+        *   Query without data
+        *   @param $sql query command
+        *   @param $stmt
+        **/
+        public function queryWithoutData($sql) {
+
+            $stmt = $this->$con->prepare($sql);
+
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        //close db connection
+        private function close() {
+
+            $this->$con = null;
+        }
+    }
+?>
