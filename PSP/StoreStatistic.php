1c0170557458256f9e136c28c08cc6aa62ec170d
function86437 <function86437@gmail.com>
2015-04-01 23:08:03 +0800

    try to store mean and standard into DB

---
+++ PSP/StoreStatistic.php
@@ -0,0 +1,40 @@
+<?php
+
+    if(isset($_POST['input']) && isset($_POST['size'])){
+
+        $input = $_POST['input'];
+        $count = 0;
+        $size = $_POST['size'];
+
+        $single = new SinglelinkedList();
+        $caculate = new CaculateMnS();
+
+        while( $count < $size) {
+            $single->add($input[$count]);
+        }
+
+        $mean = $caculate->caculateMean($single, $size);
+        $standard = $caculate->caculateStandard($single, $size, $mean);
+
+        require_once __DIR__ . '/DBConnect.php';
+
+        //new PDO connection
+        $dbcon = new DBConncet();
+
+        $sql = "INSERT INTO Mean_standard("mean","standard") VALUES (?, ?)";
+
+        $resultInsert = $dbcon->query($sql, array($mean, $standard);
+
+
+        $sql = "INSERT INTO Real_number("content") VALUES (?)";
+
+
+
+    } else {
+        echo 'Data not posted';
+    }
+
+
+
+
+?>
