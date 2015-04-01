05cd0061c46055523fb2167b073e1ca8f452e681
function86437 <function86437@gmail.com>
2015-04-01 23:07:37 +0800

    add a input box which contains size of input boxs

--- PSP/motion.js
+++ PSP/motion.js
@@ -1,9 +1,17 @@
+/**
+*   Auto new input box when user click "new" button
+*   @version 0.1
+**/
+
+//count amount of input box
 var counter = 0;
+
+var top = document.getElementById("top");
 
 document.getElementById("add").addEventListener("click", function(){
 
     var input = document.createElement("input");
-    input.setAttribute("id",counter);
+    input.setAttribute("name","input[]");
     input.style.border = "2px solid #888888";
     input.style.textAlign = "center";
     input.style.width = "40px";
@@ -14,7 +22,15 @@ document.getElementById("add").addEventListener("click", function(){
     label.style.textAlign = "center";
     label.innerHTML = "實數" + counter;
     counter++;
-    document.getElementById("top").appendChild(input);
-    document.getElementById("top").appendChild(label);
+    top.appendChild(input);
+    top.appendChild(label);
 });
 
+
+var size = document.createElement("input");
+
+size.innerHTML = counter;
+
+size.setAttribute("name", "size");
+
+top.appendChild(size);
