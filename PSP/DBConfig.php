396383cc0aa14d6d76dc6bf518efeeab9c829e65
function86437 <function86437@gmail.com>
2015-04-02 12:01:47 +0800

    DB config

---
+++ PSP/DBConfig.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    *   All database connection varisbles
+    *   @version 1.0
+    */
+    define("DB_USER","");
+    define("DB_PASSWD","");
+    define("DB_DATABASE","mean_standard");
+    define("DB_SERVER","localhost");
+    define("DB_TYPE","mysql");
+    define("DB_SERVER_CONFIG",DB_TYPE . ":host=" . DB_SERVER . ";dbname=" . DB_DATABASE . ";");
+
+?>
